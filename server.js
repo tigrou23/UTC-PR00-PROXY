cf4aa42e09b8f480a3a5afc907bd7207579debf4
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2024-05-23 11:06:28 +0200

    add cors to project

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
